94cbdd52c2f18473afd1b3d32a66c77086f1d8c8
Mikhail Shevelev <mishka2017@gmail.com>
2023-07-31 03:32:44 +0400

    tailwind css

--- fe-sdd/src/App.tsx
+++ fe-sdd/src/App.tsx
@@ -20,6 +20,9 @@ const App: React.FC = () => {
 
   return (
     <div>
+      <h1 className="text-3xl font-bold underline bg-slate-400">
+      Hello world!
+    </h1>
       <h1>Request Sender</h1>
       <UrlInput />
       <LoraInfoComponent />
--- fe-sdd/src/index.tsx
+++ fe-sdd/src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App';
+import './index.css'
 
 // As of React 18
 const root = ReactDOM.createRoot(document.getElementById('root')!)
